30a074256968740eed7bd610a42041bb3143d12e
Mireia Chaler <mireiachaler@Mireias-Mac-mini.local>
2017-10-09 14:16:44 +0200

    updated widget order and display for all post types

--- eduhack-projects-custom-widget.php
+++ eduhack-projects-custom-widget.php
@@ -35,7 +35,6 @@ class wpb_widget extends WP_Widget {
 
         global $post;
 
-        if ($post->post_type == 'cpt_project'){
 
             $title = apply_filters( 'widget_title', $instance['title'] );
 
@@ -48,6 +47,84 @@ class wpb_widget extends WP_Widget {
             $stored_meta = get_post_meta( $post->ID );
 
             ?>
+
+            <!-- STATUS -->
+            <section id="project-status" class="project-section" >
+                <h3><?php _e("Status");?></h3>
+                <div class="checkout-wrap">
+                    <ul class="checkout-bar">
+                        <?php
+                        $status = unserialize($stored_meta['widget-status'][0]);
+                        $status = unserialize($status);
+
+                        $status_names = array(1=>"Empatitzar", 2=>"Definir", 3=>"Idear", 4=>"Prototipar");
+
+                        for($s=1; $s<=4; $s++){
+                            $checked = (in_array((string)$s, $status, true)) ? 'active' : 'next';
+                            $current = ($s == $stored_meta['widget-config_fase'][0]) ? 'current' : '';
+                            ?>
+                            <li class="project-fase fase <?php echo $checked;?> <?php echo $current;?>"><span class="status-title"><?php echo $status_names[$s];?></span></li>
+                        <?php }?>
+                        <!--<div class="status-legend">
+                            <p>Objectiu: Fase <?php echo $stored_meta['widget-config_fase'][0];?></p>
+                        </div>-->
+                    </ul>
+                </div>
+            </section>
+
+
+
+            <!-- TAGS -->
+            <section id="project-tags" class="project-section" >
+                <h3><?php _e("Tags");?></h3>
+                <?php
+                $selected_tags1_text = unserialize($stored_meta['widget-tags1_text'][0]);
+                $selected_tags1_text = unserialize($selected_tags1_text);
+                $selected_tags1_img = unserialize($stored_meta['widget-config_tag_img'][0]);
+                $selected_tags1_img = unserialize($selected_tags1_img);
+
+                $selected_tags2_text = unserialize($stored_meta['widget-tags2_text'][0]);
+                $selected_tags2_text = unserialize($selected_tags2_text);
+                $selected_tags2_color = unserialize($stored_meta['widget-tags2_color'][0]);
+                $selected_tags2_color = unserialize($selected_tags2_color);
+
+                if(!empty($selected_tags1_text)){?>
+                    <div class="block-tags">
+                        <?php
+                        for($i=0; $i<count($selected_tags1_text); $i++){
+                            $this_image = wp_get_attachment_image_src( $selected_tags1_img[$i], array(100, 100) );
+                            ?>
+                            <div class="choosen-tag tag1">
+                                <img class="tag-img" src="<?php echo $this_image[0]; ?>"/>
+                                <span class="text-tag"><?php echo $selected_tags1_text[$i];?></span>
+                            </div>
+                        <?php }?>
+                    </div>
+                <?php }
+                if(!empty($selected_tags2_text)){?>
+                    <div class="block-tags">
+                        <?php
+                        for($i=0; $i<count($selected_tags2_text); $i++){?>
+                            <div class="choosen-tag">
+                                <span class="color-tag" style="background-color:#<?php echo $selected_tags2_color[$i];?>"></span>
+                                <span class="text-tag"><?php echo $selected_tags2_text[$i];?></span>
+                            </div>
+                        <?php }?>
+                    </div>
+                <?php }?>
+            </section>
+
+
+
+            <!-- DESCRIPTION -->
+            <section id="project-description" class="project-section" >
+                <h3><?php _e("Description");?></h3>
+                <div class="description"><?php echo $stored_meta['widget-description'][0]; ?></div>
+                <span class="read-more read-button"><?php _e("Read More");?></span>
+            </section>
+
+
+        
             <!-- TEAM -->
             <section id="project-team" class="project-section" >
                 <h3><?php _e("Team");?></h3>
@@ -101,79 +178,6 @@ class wpb_widget extends WP_Widget {
             </section>
 
 
-            <!-- TAGS -->
-            <section id="project-tags" class="project-section" >
-                <h3><?php _e("Tags");?></h3>
-            <?php
-            $selected_tags1_text = unserialize($stored_meta['widget-tags1_text'][0]);
-            $selected_tags1_text = unserialize($selected_tags1_text);
-            $selected_tags1_img = unserialize($stored_meta['widget-config_tag_img'][0]);
-            $selected_tags1_img = unserialize($selected_tags1_img);
-
-            $selected_tags2_text = unserialize($stored_meta['widget-tags2_text'][0]);
-            $selected_tags2_text = unserialize($selected_tags2_text);
-            $selected_tags2_color = unserialize($stored_meta['widget-tags2_color'][0]);
-            $selected_tags2_color = unserialize($selected_tags2_color);
-
-            if(!empty($selected_tags1_text)){?>
-                <div class="block-tags">
-                <?php
-                for($i=0; $i<count($selected_tags1_text); $i++){
-                    $this_image = wp_get_attachment_image_src( $selected_tags1_img[$i], array(100, 100) );
-                    ?>
-                    <div class="choosen-tag tag1">
-                        <img class="tag-img" src="<?php echo $this_image[0]; ?>"/>
-                        <span class="text-tag"><?php echo $selected_tags1_text[$i];?></span>
-                    </div>
-                <?php }?>
-                </div>
-            <?php }
-            if(!empty($selected_tags2_text)){?>
-                <div class="block-tags">
-                    <?php
-                for($i=0; $i<count($selected_tags2_text); $i++){?>
-                    <div class="choosen-tag">
-                        <span class="color-tag" style="background-color:#<?php echo $selected_tags2_color[$i];?>"></span>
-                        <span class="text-tag"><?php echo $selected_tags2_text[$i];?></span>
-                    </div>
-                <?php }?>
-                </div>
-            <?php }?>
-            </section>
-
-
-            <!-- DESCRIPTION -->
-            <section id="project-description" class="project-section" >
-                <h3><?php _e("Description");?></h3>
-                <div class="description"><?php echo $stored_meta['widget-description'][0]; ?></div>
-                <span class="read-more read-button"><?php _e("Read More");?></span>
-            </section>
-
-
-            <!-- STATUS -->
-            <section id="project-status" class="project-section" >
-                <h3><?php _e("Status");?></h3>
-                <div class="checkout-wrap">
-                    <ul class="checkout-bar">
-                    <?php
-                    $status = unserialize($stored_meta['widget-status'][0]);
-                    $status = unserialize($status);
-
-                    $status_names = array(1=>"Empatitzar", 2=>"Definir", 3=>"Idear", 4=>"Prototipar");
-
-                    for($s=1; $s<=4; $s++){
-                        $checked = (in_array((string)$s, $status, true)) ? 'active' : 'next';
-                        $current = ($s == $stored_meta['widget-config_fase'][0]) ? 'current' : '';
-                        ?>
-                        <li class="project-fase fase <?php echo $checked;?> <?php echo $current;?>"><span class="status-title"><?php echo $status_names[$s];?></span></li>
-                    <?php }?>
-                    <!--<div class="status-legend">
-                        <p>Objectiu: Fase <?php echo $stored_meta['widget-config_fase'][0];?></p>
-                    </div>-->
-                    </ul>
-                </div>
-            </section>
-
 
             <!-- BUTTONS -->
             <section id="project-buttons" class="project-section" >
@@ -193,7 +197,7 @@ class wpb_widget extends WP_Widget {
             <?php
 
             echo $args['after_widget'];
-        }
+
 
 
     }
